e780eb15c068c99ad94d7dd9f5f6719543092b32
Konrad Kościelski <konrad.koscielski@interia.pl>
2018-01-10 11:13:09 +0100

    initial

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+var a = prompt('Podaj wartosc a', a);
+var b = prompt('Podaj wartosc b', b);
+
+var value =  (a * a) - (2 * a * b) + (b * b);
+alert('Twoj wynik to: ' +value);
+console.log('Twoj wynik to: ' +value);
+if (value > 0) {
+    alert('Twoj wynik jest dodatni')
+    console.log('Twoj wynik jest dodatni');
+} else if (value < 0) {
+    alert('Twoj wynik jest ujemny')
+    console.log ('Twoj wynik jest ujemy');
+} else if (value == 0) {
+    alert('0 jest nieujemne ani niedodatnie, chyba');
+    console.log ('0 jest nieujemne ani niedodatnie, chyba');
+}
